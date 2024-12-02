7acb3e80a9328811be5d9a76577e7b91c6780a1d
GenisonLeal <genisonleal@gmail.com>
2024-12-02 12:05:15 -0300

    Aditional features2

--- lib/domain/services/contact_service.dart
+++ lib/domain/services/contact_service.dart
@@ -1,5 +1,3 @@
-// ignore_for_file: unnecessary_null_comparison
-
 import 'package:app_facul/domain/entities/contact.dart';
 import 'package:app_facul/domain/exception/domain_layer_exception.dart';
 import 'package:app_facul/domain/interfaces/contact_dao.dart';
