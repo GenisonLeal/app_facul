4ec483cd300ca7d77e132fb4b3d7138aece75810
GenisonLeal <genisonleal@gmail.com>
2024-12-09 21:25:02 -0300

    new

--- lib/domain/services/contact_service.dart
+++ lib/domain/services/contact_service.dart
@@ -25,30 +25,24 @@ validateName(String name){
   var min = 3; 
   var max = 50; 
 
-  if(name == null){
-    throw DomainLayerException('O nome é obrigatório.');
-  }else if(name.length < min){
-    throw DomainLayerException('O nome deve possuir pelo menos $min caracteres.');
-  }else if(name.length > max){
-    throw DomainLayerException('O nome deve possuir no máximo $max caracteres.');
-  }
+  if(name.length < min){
+  throw DomainLayerException('O nome deve possuir pelo menos $min caracteres.');
+}else if(name.length > max){
+  throw DomainLayerException('O nome deve possuir no máximo $max caracteres.');
+}
 }
 // e-mail → seja obrigatório e tenha @ 
 validateEmail(String email){
-  if(email == null){
-    throw DomainLayerException('O e-mail é obrigatório.');
-  }else if(!email.contains('@')){
-    throw DomainLayerException('O e-mail deve possuir @.');
-  }
+  if(!email.contains('@')){
+  throw DomainLayerException('O e-mail deve possuir @.');
+}
 }
 // telefone → seja obrigatório e tenha o formato (99) 9 9999-9999 
 validatePhone(String phone){
   var format = RegExp(r'^\([1-9]{2}\) [9] [6-9]{1}[0-9]{3}\-[0-9]{4}$'); // (99) 9 9999-9999
-  if(phone == null){
-    throw DomainLayerException('O telefone é obrigatório.');
-  }else if(!format.hasMatch(phone)){
-    throw DomainLayerException('Formato inválido. O formato deve ser (99) 9 9999-9999.');
-  }
+  if(!format.hasMatch(phone)){
+  throw DomainLayerException('Formato inválido. O formato deve ser (99) 9 9999-9999.');
+}
 }
 
 
